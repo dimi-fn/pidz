1014b3b51c8f15bf572d8c7b18a62ea66efc0503
Zeiadork <zeiagillies@gmail.com>
2022-03-07 18:14:16 +0000

    Getting comments and journals working

--- client/js/index.js
+++ client/js/index.js
@@ -1,13 +1,10 @@
 // Create array of all input buttons of type submit wrapped in a form
 const button = document.querySelectorAll("form input[type ='submit']");
 
-//check length for testing
-console.log("button array length: " +button.length);
 
 // Add event listeners to each button in button array
 button.forEach( function (thisButton)
 {
-    console.log("Adding event listener for " +thisButton.id)
     thisButton.addEventListener('click', buttonHandler);
 });
 
@@ -24,13 +21,11 @@ function buttonHandler (submitEvent)
     //If choice === testSubmitJournal  DO STUFF
     if (choice === 'testSubmitJournal')
     {
-        console.log("submit clicked");
         sumbitJournal();
     }
     // If choice === refreshJournals // do stuff
     else if (choice === 'refreshJournals')
     {
-        console.log("refresh clicked");
         showAllJournals();
     }
     else
@@ -63,22 +58,60 @@ function sumbitJournal()
 };
 
 
-function showAllJournals()
+async function showAllJournals()
 {
+    let journalData = await getJournals();
+    let commentData = await getComments();
 
-    fetch("http://localhost:3000/allJournals")
-    .then( (resp) => resp.json())
-    .then( (data) => 
+
+    console.log("Journal data: " +journalData)
+    console.log("Journal data: " +commentData)
+
+
+    journalData.forEach((jrnl) =>
     {
-        data.forEach((item) =>
-        {
-            let paragraph = document.createElement("p");
-            let paragraphContent = document.createTextNode(JSON.stringify(item.content));
+        let paragraph = document.createElement("p");
+        let paragraphContent = document.createTextNode(JSON.stringify(jrnl.content));
+        paragraph.appendChild(paragraphContent);
+        document.getElementById("displayJournalsSection").appendChild(paragraph );
 
-            paragraph.appendChild(paragraphContent);
-            document.getElementById("displayJournalsSection").appendChild(paragraph );
+        commentData.forEach((cmt) =>
+        {
+            if (cmt.journalId === jrnl.id)
+            {
+                let cmtBlock = document.createElement("blockquote");
+                let cmtContent = document.createTextNode(JSON.stringify(cmt.content));
+                cmtBlock.appendChild(cmtContent)
+                document.getElementById("displayJournalsSection").appendChild(cmtBlock);
+            }
         });
 
-    });
 
+    });
+  
+}
+
+
+
+function getJournals ()
+{
+    return fetch("http://localhost:3000/allJournals")
+        .then( (resp) => resp.json())
+        .then( (jData) => 
+        {
+            return jData
+        })
+        .catch((error) => alert ("Couldn't get journals, reason: " +error));
+}
+
+
+function getComments ()
+{
+    return fetch("http://localhost:3000/allComments")
+        .then( (resp) => resp.json())
+        .then( (cData) => 
+        {
+            return cData
+        })
+        .catch((error) => alert ("Couldn't get comments, reason: " +error));
 }
