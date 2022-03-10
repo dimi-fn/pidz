03ae6ccfcbcbd0f25ba18a562fea2ecedf62c3eb
Paul Dobrescu <paul.dobrescu10@gmail.com>
2022-03-10 18:42:50 +0000

    Styling and Navbar added

--- client/js/index.js
+++ client/js/index.js
@@ -882,4 +882,22 @@ async function numPages()
     return Math.ceil( journalsLength/ journalsPerPage);
 }
 
+
+window.onscroll = function() {myFunction()};
+
+
+var navbar = document.getElementById("navbar");
+
+
+var sticky = navbar.offsetTop;
+
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 // END OF CODE
